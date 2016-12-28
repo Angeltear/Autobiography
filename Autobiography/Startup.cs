619f46fade905b66580f7ac6e543dce739ce2cf8
White Wolf <zero_zero@abv.bg>
2016-12-28 15:24:23 +0200

    Add project files.

---
+++ Autobiography/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Autobiography.Startup))]
+namespace Autobiography
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
